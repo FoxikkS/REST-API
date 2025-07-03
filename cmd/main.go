4f22427b1a53369f09cc96fe410ada7ae2ec1d20
Andrey <andrejpolisuk7502@gmail.com>
2025-07-04 00:44:46 +0500

    feat(logging): add loger for local, dev, prod

--- cmd/main.go
+++ cmd/main.go
@@ -3,9 +3,35 @@ package main
 import (
 	"REST-API-pet-proj/internal"
 	"fmt"
+	"log"
+	"log/slog"
+	"net/http"
+	"os"
+)
+
+const (
+	envLocal = "local"
+	envDev   = "dev"
+	envProd  = "prod"
 )
 
 func main() {
 	cfg := internal.InitConfigParser()
-	fmt.Printf("%+v\n", cfg)
+}
+
+func LoggerHandler(env string) *slog.Logger {
+	var logger *slog.Logger
+	switch env {
+	case envLocal:
+		slog.New(
+			slog.NewTextHandler(os.Stdout, &slog.HandlerOptions{Level: slog.LevelDebug}),
+		)
+	case envDev:
+		slog.New(
+			slog.NewJSONHandler(os.Stdout, &slog.HandlerOptions{Level: slog.LevelDebug}),
+		)
+	case envProd:
+		slog.NewJSONHandler(os.Stdout, &slog.HandlerOptions{Level: slog.LevelInfo})
+	}
+	return logger
 }
