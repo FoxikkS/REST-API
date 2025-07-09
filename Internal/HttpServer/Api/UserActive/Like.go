02e1c6fa036113ba1ae574f00b93994da1c3fbc5
Andrey <andrejpolisuk7502@gmail.com>
2025-07-09 16:38:13 +0500

    upd

--- Internal/HttpServer/Api/UserActive/Like.go
+++ Internal/HttpServer/Api/UserActive/Like.go
@@ -1,1 +1,12 @@
 package UserActive
+
+import (
+	"REST-API-pet-proj/Internal/Storage/Sqlite"
+	"net/http"
+)
+
+func PutALike(storage *Sqlite.Storage) http.HandlerFunc {
+	return func(w http.ResponseWriter, r *http.Request) {
+		
+	}
+}
